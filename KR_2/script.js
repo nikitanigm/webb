565e4aa7ac5c2a03ff9627b8c7c2ab2e16e9da59
nikitanigm <93225138+nikitanigm@users.noreply.github.com>
2021-12-12 22:00:21 +0200

    dfgdfg

--- KR_2/script.js
+++ KR_2/script.js
@@ -1,9 +1,47 @@
 var op; //выбранный оператор
 function func() {
-  var result;
-  var num1 = Number(document.getElementById("num1").value);
-  var num2 = Number(document.getElementById("num2").value);
+  var result = '';
   switch (op) {
+    case '1':
+      result += '1';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '2':
+      result += '2';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '3':
+      result += '3';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '4':
+      result += '4';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '5':
+      result += '5';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '6':
+      result += '6';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '7':
+      result += '7';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '8':
+      result += '8';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '9':
+      result += '9';
+      document.getElementById("num1").innerHTML = result;
+      break;
+    case '0':
+      result += '0';
+      document.getElementById("num1").innerHTML = result;
+      break;
     case '+':
       result = num1 + num2;
       break;
--- LR_10/script.js
+++ LR_10/script.js
@@ -1,7 +1,7 @@
 var i = -1
 let imagesUrls = ["./images/doge.jpg","./images/1.png","./images/2.png","./images/moln.jpg","./images/car.jpg"]
-document.getElementById('imageButton').addEventListener("click", setRan-domImage)
-docu-ment.getElementById('rng').addEventListener("change",()=>{document.getElementById("txt").value = document.getElementById("rng").value})
+document.getElementById('imageButton').addEventListener("click", setRandomImage)
+document.getElementById('rng').addEventListener("change",()=>{document.getElementById("txt").value = document.getElementById("rng").value})
 document.getElementById('slideshowBtn').addEventListener("click", ()=>{
         setInterval(()=>{
             setRandomImage();
@@ -12,7 +12,7 @@ function setRandomImage()
     let randomIndex = (imagesUrls.length-1) * Math.random();
     if (i!=randomIndex)
     {
-        docu-ment.getElementById('controlImage').setAttribute('src',imagesUrls[randomIndex.toFixed()])
+        document.getElementById('controlImage').setAttribute('src',imagesUrls[randomIndex.toFixed()])
         i = randomIndex
     }
 }
